6441a0327160712646c2459a35447ac2c307fe3c
tusheer <jaanealamtusher@gmail.com>
2020-12-16 14:47:00 +0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+type AdressObj = {
+    addressNo: number;
+    roadAddress: string;
+    city: string;
+    state: string;
+    country: string;
+};
+
+const addressToObject = (address: string): AdressObj => {
+    return {};
+};
+
+const makeShortAddress = ({ addressNo, roadAddress, city, state, country }: AdressObj) => {};
